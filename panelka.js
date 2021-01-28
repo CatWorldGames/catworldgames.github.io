20fab76ad6c7accdfef29bdea438a7dee677004e
CatWorldGames <76588958+CatWorldGames@users.noreply.github.com>
2021-01-28 16:16:28 +0300

    Add files via upload

---
+++ panelka.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA_m0u9H45WPFhCSo0hUJzZ0R8P2zLLRoQ",
+    authDomain: "nero-browser-10070.firebaseapp.com",
+    databaseURL: "https://nero-browser-10070.firebaseio.com",
+    projectId: "nero-browser-10070",
+    storageBucket: "nero-browser-10070.appspot.com",
+    messagingSenderId: "795294806260",
+    appId: "1:795294806260:web:db1729849f3c4649e62ea1",
+    measurementId: "G-7DHFZCQX83"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
